3868abd2ff170b8a4f46e0be9e548e8d7d662309
DaniloGabrielGuedes <danilo.guedes@lab1.com.br>
2022-02-27 16:38:06 -0300

    fix(Index): token e canais de testes + MessageEmbed's

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({ partials: ["CHANNEL"], intents: [
     Intents.FLAGS.GUILDS, 
     Intents.FLAGS.GUILD_MESSAGES,
@@ -6,7 +6,7 @@ const client = new Client({ partials: ["CHANNEL"], intents: [
     Intents.FLAGS.DIRECT_MESSAGE_TYPING    
 ] });
 const config = require("./global/config.json");
-const token = 'NzYzNjkzNjM1MTY5NjgxNDE4.X37bHw.rPgB_tjR5XLXzO8XTm9IxHH6_sY';
+const token = 'ODQxMzExOTc1NzIzMzY4NTA5.YJk61Q.C-v5spyv5NlPgSqC-SAmNP5xQB4';
 const axios = require("axios");
 var aToken = "";
 
@@ -53,49 +53,49 @@ client.on('ready', () => {
         const date = new Date();
         date.setHours(date.getHours() - 3); // fuso horario do Brasil
 
-        if ((date.getHours() === 08 && date.getMinutes() === 35) && date.getDay() != 0 && date.getDay() != 1 && date.getDay() != 5 && date.getDay() != 6) { // Diferente de domingo, segunda, sexta e sabado
+        // if ((date.getHours() === 08 && date.getMinutes() === 35) && date.getDay() != 0 && date.getDay() != 1 && date.getDay() != 5 && date.getDay() != 6) { // Diferente de domingo, segunda, sexta e sabado
 
-            client.channels.fetch("753276752192602264").then(channel => { // CANAL PRIORIDADES DIÁRIAS LAB1 SERVER
+        //     client.channels.fetch("841310589958881283").then(channel => { // CANAL PRIORIDADES DIÁRIAS LAB1 SERVER
 
-                const ti = channel.guild.roles.cache.find(role => role.name == "TI");
-                const messages = [
-                    "Reunião diária galerinha do " + `${ti}` + ".",
-                    "Camarão que dorme a onda leva então bora " + `${ti}` + ", reunião!",
-                    "Vamo acordar, vamo acordar, bora pra reunião.",
-                    "Olha o tempo cidadão, já é hora da reunião.",
-                    "```if (DateTime.Now.Time == '08:40') \r\n   print(\"É hora da reunião!\") ```",
-                    "```SELECT CASE WHEN CONVERT(CHAR(5), GETDATE(), 108) = '08:40' THEN 'Reunião diária' ELSE 'Anda logo' END Mensagem```",
-                    "```<html>\r\n  <head> Reunião diária </head> \r\n  <body> Anda logo rapaiz </body> \r\n</html>```",
-                    ":robot: R E U N I Ã O - D I Á R I A :robot:",
-                    "https://TIdaLAB1.com/pt-br/hora?de=Reuniao%20Diaria",
-                    "Se arrume e ligue câmera, já é hora da reunião.",
-                    "Já pegou seu café?",
-                    "De novo na mesma tarefa???",
-                    "01010010 01100101 01110101 01101110 01101001 11100011 01101111 00100000 01100100 01101001 11100001 01110010 01101001 01100001"
-                ]
-                const randomMessage = messages[Math.floor(Math.random() * messages.length)];
-                var msg = randomMessage.includes(`${ti}`) ? "**Bom dia!** \r\n" + randomMessage : "**Bom dia " + `${ti}` + "** \r\n" + randomMessage;
+        //         const ti = channel.guild.roles.cache.find(role => role.name == "TI");
+        //         const messages = [
+        //             "Reunião diária galerinha do " + `${ti}` + ".",
+        //             "Camarão que dorme a onda leva então bora " + `${ti}` + ", reunião!",
+        //             "Vamo acordar, vamo acordar, bora pra reunião.",
+        //             "Olha o tempo cidadão, já é hora da reunião.",
+        //             "```if (DateTime.Now.Time == '08:40') \r\n   print(\"É hora da reunião!\") ```",
+        //             "```SELECT CASE WHEN CONVERT(CHAR(5), GETDATE(), 108) = '08:40' THEN 'Reunião diária' ELSE 'Anda logo' END Mensagem```",
+        //             "```<html>\r\n  <head> Reunião diária </head> \r\n  <body> Anda logo rapaiz </body> \r\n</html>```",
+        //             ":robot: R E U N I Ã O - D I Á R I A :robot:",
+        //             "https://TIdaLAB1.com/pt-br/hora?de=Reuniao%20Diaria",
+        //             "Se arrume e ligue câmera, já é hora da reunião.",
+        //             "Já pegou seu café?",
+        //             "De novo na mesma tarefa???",
+        //             "01010010 01100101 01110101 01101110 01101001 11100011 01101111 00100000 01100100 01101001 11100001 01110010 01101001 01100001"
+        //         ]
+        //         const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+        //         var msg = randomMessage.includes(`${ti}`) ? "**Bom dia!** \r\n" + randomMessage : "**Bom dia " + `${ti}` + "** \r\n" + randomMessage;
 
-                msg += "\r\nReunião no link https://meet.google.com/eqo-vkby-qyb";
+        //         msg += "\r\nReunião no link https://meet.google.com/eqo-vkby-qyb";
 
-                channel.send(msg);
-            })
-        }
+        //         channel.send(msg);
+        //     })
+        // }
 
-        if ((date.getHours() === 11 && date.getMinutes() === 30) && date.getDay() == 1) {
-            client.channels.fetch("684017942307930147").then(channel => { // CANAL GERAL LAB1 SERVER
-                channel.send("**Bom dia @everyone!**");
+        // if ((date.getHours() === 11 && date.getMinutes() === 30) && date.getDay() == 1) {
+            // client.channels.fetch("841310589958881283").then(channel => { // CANAL GERAL LAB1 SERVER
+            //     channel.send("**Bom dia @everyone!**");
 
-                const embed = new Discord.MessageEmbed()
-                .setColor(0xffc9a0)
-                .setTitle("**Não se esqueçam de preencher a pesquisa de engajamento.**")
-                .setDescription("**__[https://teamculture.com.br](https://app.teamculture.com.br/survey)__**")
-                .setTimestamp()
-                .setFooter({ text: "LAB1 Consultoria", iconURL: "https://cdn.discordapp.com/icons/684017942307930142/9fe6c340d4ca777e6792c7aee8fbc36e.webp?size=1024"})
-                .setThumbnail("https://d2kq0urxkarztv.cloudfront.net/5d07af2a6d9d5501de3dac39/2530029/upload-ea23c1c9-a8e6-4251-a920-5a390f1e7f0a.png?e=webp&nll=true")
-                channel.send({ embeds: [embed] });
-            })
-        }
+            //     const embed = new MessageEmbed()
+            //     .setColor(0xffc9a0)
+            //     .setTitle("**Não se esqueçam de preencher a pesquisa de engajamento.**")
+            //     .setDescription("**__[https://teamculture.com.br](https://app.teamculture.com.br/survey)__**")
+            //     .setTimestamp()
+            //     .setFooter({ text: "LAB1 Consultoria", iconURL: "https://cdn.discordapp.com/icons/684017942307930142/ba8f171a7a72b409f9a772ff046fa62d.webp"}) //2048
+            //     .setThumbnail("https://d2kq0urxkarztv.cloudfront.net/5d07af2a6d9d5501de3dac39/2530029/upload-ea23c1c9-a8e6-4251-a920-5a390f1e7f0a.png?e=webp&nll=true")
+            //     channel.send({ embeds: [embed] });
+            // })
+        // }
 
         // if ((date.getHours() === 13 && date.getMinutes() === 30) && date.getDay() == 5) {
         //     client.channels.fetch("684017942307930147").then(channel => { // CANAL GERAL LAB1 SERVER 
