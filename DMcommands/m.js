ed1457038b32cef7331cdb902e1dbea523d51de5
Danilo Gabriel Guedes <danilo.gabriel.guedes@gmail.com>
2022-04-06 19:07:26 -0300

    feat: tentando filtrar array de duas dimensões
    só tentando mesmo

--- DMcommands/m.js
+++ DMcommands/m.js
@@ -1,28 +1,61 @@
 const { MessageEmbed } = require('discord.js');
 const config = require("../global/config.json");
 
-var task = [];
+var tasks = [];
 
 module.exports.run = async(client, message, args) => {
 
     var date = new Date();
     date.setHours(date.getHours()) // TODO: - 3 on HEROKU 
     var dt = date.toTimeString().substring(0, 5);
-    var description = "";
-    
+    var description;
+    description = "";
     try {
-        task.push("**" + dt + "** - " + message.content.slice(config.prefix.length).replace("m ", ""));
+        var task = "**" + dt + "** - " + message.content.slice(config.prefix.length).replace("m ", "");
         
+        tasks.push([message.author.id, task]);
+                
         const embed = new MessageEmbed()
             .setColor(0xffc9a0)
             .setTitle("**Aqui está o seu resumo de horarios do dia.**")
             .setTimestamp()
             .setFooter({ text: "LAB1 Consultoria", iconURL: "https://cdn.discordapp.com/icons/684017942307930142/ba8f171a7a72b409f9a772ff046fa62d.webp" })
+
+            // for(var i = 0; i < tasks.length; i++){
+
+            //     var obj = tasks[i];
+            //     console.log(1);
+                
+            //     for(var key in obj){
+
+            //         console.log(2);
+            //         console.log(obj[key]);
+
+            //         if(obj[key] == message.author.id){
+
+            //             console.log(3);
+            //             description += tasks[key][key.length];
+            //             continue;
+            //         }
+            //     }
+            // }
+
+
+        // for (let index = 0; index < tasks.length; index++) {
             
-            for (let index = 0; index < task.length; index++) {
-                description += task[index] + "\n";
-            }        
-            embed.setDescription(description)
+        //     console.log(tasks[index]);
+
+        //     if (tasks[index] == message.author.id)
+        //     {
+        //         for (let x = 0; index < tasks[i].length; x++) {
+                    
+        //             description += tasks[index][i] + "\n";
+                    
+        //         }
+        //     }
+        // }
+        
+        embed.setDescription(description)
 
         message.channel.send({ embeds: [embed] });
 
